8eac5767ca43b11272de16b3756cfe5fe2a16de3
Matt-wzy <wwwa_2012@126.com>
2021-02-17 17:49:05 +0800

    Make the boot output some code to make sure it works.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
+import telegram
+import socket
+import socks
+
+def networkinit():
+    port = int(input("端口:"))
+    socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", port)
+    socket.socket = socks.socksocket
+
+def gettokenfrom(input_A):
+    if input_A == "keydown":
+        TOKEN = input("TOKEN：")
+        return TOKEN
 
 def main ():
-    pass
+    TOKEN = gettokenfrom("keydown")
+    bot = telegram.Bot(token=TOKEN)
+    print(bot.get_me())
 
 if __name__ == "__main__":
     # execute only if run as a script
+    networkinit()
     main()
