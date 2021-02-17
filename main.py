5324d2226753b04ce0bfb7a989e4966b49e7ea6a
Matt-wzy <wwwa_2012@126.com>
2021-02-17 22:02:36 +0800

    Auto set the port to the dufult settings

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import socket
 import socks
 
 def networkinit():
-    port = int(input("端口:"))
+    port = input("初始化网络，本地代理端口(默认7891):")
+    if (port == 0 or port == "") :
+        port = 7891
+    port = int(port)
+    print("OK, setting to", port)
     socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", port)
     socket.socket = socks.socksocket
 
 def gettokenfrom(input_A):
     if input_A == "keydown":
-        TOKEN = input("TOKEN：")
+        TOKEN = input("tgBot TOKEN：")
         return TOKEN
 
 def main ():
@@ -20,7 +24,16 @@ def main ():
     bot = telegram.Bot(token=TOKEN)
     print(bot.get_me())
 
+
+def printhello():
+    hello='''你好，欢迎下载我的代码并运行
+本代码所有功能仅基于本人对于下载转发以及telegram bot的拙见
+项目基于telegrambot框架
+创建者：github.com/Matt-wzy'''
+    print(hello)
+
 if __name__ == "__main__":
     # execute only if run as a script
+    printhello()
     networkinit()
     main()
